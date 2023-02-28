0ff8854914a202d0f57960e7fec4552ebf9fd87a
mehmed-damak <65054270+mehmed-damak@users.noreply.github.com>
2023-02-28 17:45:37 -0500

    run js test code

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function add7(number){
+    return number + 7;
+}
+console.log(add7(2));
+function multiply(num1, num2){
+    return num1*num2;
+}
+console.log(multiply(3, 4));
+function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+}
+console.log(capitalize("dokg"));
+function lastLetter(string){
+    return string.charAt(string.length-1);
+}
+console.log (lastLetter("abcd"));
